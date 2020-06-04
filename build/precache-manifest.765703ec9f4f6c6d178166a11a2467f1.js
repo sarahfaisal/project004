3545cb385cc947b62a71288f60ad1e5bfde3fe11
sarahfaisal <faisalsara124@gmail.com>
2020-06-04 23:20:15 +0000

    second commit

---
+++ build/precache-manifest.765703ec9f4f6c6d178166a11a2467f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba61d37efd0303f71cb7d9ae1c8fc46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9003281d2fd150f87447",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2c63779f4333f8b1b73",
+    "url": "/static/js/2.316b972e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.316b972e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9003281d2fd150f87447",
+    "url": "/static/js/main.4ac5a84b.chunk.js"
+  },
+  {
+    "revision": "55213c9401a18359cfd9",
+    "url": "/static/js/runtime-main.86f6684e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
