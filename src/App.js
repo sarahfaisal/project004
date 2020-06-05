dadfa6cd5b63c3c08513573012fd1f36d0c506c3
sarahfaisal <faisalsara124@gmail.com>
2020-06-05 16:24:36 +0000

    Update App.js
    updated by sarah

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+           updated by Sarah
         </p>
         <a
           className="App-link"
