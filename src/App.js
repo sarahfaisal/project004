6c3c57f05b7421fd46ea42a39c48a2a79dbdb66c
sarahfaisal <faisalsara124@gmail.com>
2020-06-05 17:03:17 +0000

    Update App.js
    hello world added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
-           updated by Sarah faisal
+           hello from  Sarah faisal
         </p>
         <a
           className="App-link"
